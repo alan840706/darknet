80af10cbb90f94f25ddf486439d99fc5a80aa47e
alan840706 <58114247+alan840706@users.noreply.github.com>
2020-08-21 23:29:51 +0800

    Update detector.c

--- src/detector.c
+++ src/detector.c
@@ -29,7 +29,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     char *train_images = option_find_str(options, "train", "data/train.txt");
     char *valid_images = option_find_str(options, "valid", train_images);
     char *backup_directory = option_find_str(options, "backup", "/backup/");
-
+    printf("backup_directory: %s",backup_directory)
     network net_map;
     if (calc_map) {
         FILE* valid_file = fopen(valid_images, "r");
